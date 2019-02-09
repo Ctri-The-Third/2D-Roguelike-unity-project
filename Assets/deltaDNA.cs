a09aa8009b1390f6fdf43efa14e4904a1cf45067
C'tri Goudie <43475390+Ctri-The-Third@users.noreply.github.com>
2019-02-09 11:27:21 +0000

    Iniitial commit + events
    Out of the box "Completed" roguelike with
    * SDK being initialised correctly
    * Logging mission start
    * Logging mission completed

---
+++ Assets/deltaDNA.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DeltaDNA;
+namespace Completed
+{ 
+public class deltaDNA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Testing initiation of SDK " + GameManager.instance.getLevel());
+        if (GameManager.instance.getLevel() <= 1)
+        {
+            DDNA.Instance.StartSDK();
+        }
+            Debug.Log("Successful initiation? " + DDNA.Instance.isActiveAndEnabled);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
